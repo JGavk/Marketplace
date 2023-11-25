6c782abd7b4107323b103f1348b22858580bde03
Juan Pablo <juan.pablo.puerta@correounivalle.edu.co>
2023-11-25 16:02:53 -0500

    Update

--- src/view/WelcomeView.java
+++ src/view/WelcomeView.java
@@ -1,4 +1,19 @@
 package view;
 
-public class WelcomeView {
+import javax.swing.*;
+import java.awt.*;
+
+public class WelcomeView extends JFrame{
+    private JPanel mainPanel;
+    private JLabel label1;
+
+   public WelcomeView (){
+       setTitle("Product Manager");
+       setDefaultCloseOperation(EXIT_ON_CLOSE);
+       setSize(500, 700);
+       setVisible(true);
+       setContentPane(mainPanel);
+
+       label1.setPreferredSize(new Dimension(400, 200));
+   }
 }
