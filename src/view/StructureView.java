9ec98c16a36d744cef1a893f33a611f084258f39
Juan Pablo <juan.pablo.puerta@correounivalle.edu.co>
2023-11-26 18:50:36 -0500

    paneles now

--- src/view/StructureView.java
+++ src/view/StructureView.java
@@ -1,16 +1,20 @@
 package view;
 
 import javax.swing.*;
+import java.awt.*;
 
 public class StructureView extends JFrame {
 
     private JPanel mainSPanel;
+    private JPanel inputPanel;
 
     public StructureView(){
         setSize(700,700);
         setLocationRelativeTo(null);
         setVisible(true);
         setContentPane(mainSPanel);
+
+        inputPanel.setBackground(Color.black);
     }
 
 
